a9ec0494e2886326e66d9e5f63827bf8657cd675
Oniel <51058739+0niel@users.noreply.github.com>
2021-08-25 13:44:18 +0300

    Add --disable-dev-shm-usage argument

--- schedule_parser/downloader.py
+++ schedule_parser/downloader.py
@@ -204,6 +204,7 @@ class Downloader:
         chrome_options.add_argument('--window-size=1420,1080')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(chrome_options=chrome_options)
         page_sources = []
         driver.get(self._url)
