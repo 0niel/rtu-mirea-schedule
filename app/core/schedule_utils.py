ff9be04fd68ece47f12cf0b88ecbe6acc6bc4e2b
Artem <artyom2002-98@mail.ru>
2022-02-15 23:07:09 +0300

    fix return types in schedule_utils (#29)

--- app/core/schedule_utils.py
+++ app/core/schedule_utils.py
@@ -3,7 +3,7 @@ import datetime
 
 class ScheduleUtils:
     @staticmethod
-    def get_week(date: datetime.date = None) -> int:
+    def get_week(date: datetime.datetime = None) -> int:
         """Возвращает номер учебной недели по дате
 
         Args:
@@ -23,7 +23,7 @@ class ScheduleUtils:
         return week
 
     @staticmethod
-    def get_semester_start(date: datetime.date = None) -> datetime.datetime:
+    def get_semester_start(date: datetime.datetime = None) -> datetime.datetime:
         """Возвращает дату начала семестра по дате
 
         Args:
@@ -36,14 +36,14 @@ class ScheduleUtils:
             return ScheduleUtils.get_second_semester()
 
     @staticmethod
-    def now_date() -> datetime.date:
+    def now_date() -> datetime.datetime:
         moscow_offset = datetime.timezone(datetime.timedelta(hours=3))
         return datetime.datetime.now(moscow_offset)
 
     @staticmethod
-    def get_first_semester() -> datetime.date:
+    def get_first_semester() -> datetime.datetime:
         return datetime.datetime(ScheduleUtils.now_date().year, 9, 1)
 
     @staticmethod
-    def get_second_semester() -> datetime.date:
+    def get_second_semester() -> datetime.datetime:
         return datetime.datetime(ScheduleUtils.now_date().year, 2, 9)
