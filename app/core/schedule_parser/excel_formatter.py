83f2d44eb006ee5546c933313618c2539e965ff2
Oniel <51058739+0niel@users.noreply.github.com>
2022-09-01 13:22:14 +0300

    Update excel formatter typos

--- app/core/schedule_parser/excel_formatter.py
+++ app/core/schedule_parser/excel_formatter.py
@@ -188,8 +188,9 @@ class ExcelFormatter(Formatter):
     def __fix_typos(self, names: str) -> str:
         """Исправление ошибок и опечаток в документе"""
         names = re.sub(r'деятельность\s*деятельность', 'деятельность', names)
-        names = re.sub(r'^Военная$', 'Военная подготовка', names, flags=re.MULTILINE)
-        names = re.sub(r'^подготовка$', 'Военная подготовка', names, re.MULTILINE)
+        names = re.sub(r'^\s*Военная\s*$', 'Военная подготовка', names, flags=re.MULTILINE)
+        names = re.sub(r'^\s*подготовка\s*$', 'Военная подготовка', names, re.MULTILINE)
+        names = re.sub(r'^1\s*п/г,\s*2\s*п/г$', 'Военная подготовка', names, re.MULTILINE)
 
         return names
 
